fe58ccfcc000b2be3a0240781c641a218c1897f9
satur566 <satur566@gmail.com>
2020-09-23 21:25:47 +0300

    First commit.

---
+++ HappyTickets/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HappyTickets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //First commit.
+        }
+    }
+}
