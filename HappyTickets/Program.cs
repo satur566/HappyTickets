30f0d08bf3ce3986800127ea480579b12294c84b
satur566 <satur566@gmail.com>
2020-09-23 21:34:20 +0300

    First dummy solution added.

--- HappyTickets/Program.cs
+++ HappyTickets/Program.cs
@@ -8,9 +8,34 @@ namespace HappyTickets
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void MultipleForMethod()
         {
-            //First commit.
+            for (int a = 0; a < 10; a++)
+            {
+                for (int b = 0; b < 10; b++)
+                {
+                    for (int c = 0; c < 10; c++)
+                    {
+                        for (int d = 0; d < 10; d++)
+                        {
+                            for (int e = 0; e < 10; e++)
+                            {
+                                for (int f = 0; f < 10; f++)
+                                {
+                                    if((a + b + c) == (d + e +f))
+                                    {
+                                        Console.WriteLine($"{a}{b}{c}{d}{e}{f}");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {            
+            MultipleForMethod();
         }
     }
 }
