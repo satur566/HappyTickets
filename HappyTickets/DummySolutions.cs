0e26baf287eee06aeac21fcdab0d465bc1126e8b
Alexandr Maximov <satur566@gmail.com>
2020-09-30 14:50:00 +0300

    Some cleanse.

--- HappyTickets/DummySolutions.cs
+++ HappyTickets/DummySolutions.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace HappyTickets
 {
@@ -12,7 +9,6 @@ namespace HappyTickets
         public int DummiestOne(int countOfDigits)
         {
             int counter = 0;
-            int subCounter = 0;
             switch (countOfDigits)
             {
                 case 1:
--- HappyTickets/Program.cs
+++ HappyTickets/Program.cs
@@ -1,25 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HappyTickets
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             DummySolutions ds = new DummySolutions();
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
-            Console.WriteLine($"Перебор циклами нашел {ds.StringMethod(7)} билетов.");
-            stopwatch.Stop();
-            Console.WriteLine($"На это ушло {stopwatch.ElapsedMilliseconds} миллисекунд.");
-            stopwatch.Restart();
-            Console.WriteLine($"Перебор циклами нашел {ds.DummiestOne(7)} билетов.");
+            Console.WriteLine($"Перебор циклами нашел {ds.DummiestOne(11)} билетов.");
             stopwatch.Stop();
             Console.WriteLine($"На это ушло {stopwatch.ElapsedMilliseconds} миллисекунд.");
             stopwatch.Reset();
